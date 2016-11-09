8a3ab237fa8bc091da0e953630354a1254598359
Svetoslav Slavov <svetoslav_slavov@icloud.com>
2016-11-10 00:00:24 +0200

    First

---
+++ Tasks.Web/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tasks.Web.Models;
+
+namespace Tasks.Web.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public ActionResult Index()
+        {
+            var tasks = this.db.Tasks
+                .OrderBy(e => e.StartDateTime)
+                .Where(e => e.IsPublic)
+                .Select(e => new TaskViewModel()
+                {
+                    Id=e.Id,
+                    Title=e.Title,
+                    StartDateTime=e.StartDateTime,
+                    Duration=e.Duration,
+                    Author=e.Author.FullName,
+                    Location=e.Location
+                });
+            var upcomingTasks = tasks.Where(e => e.StartDateTime > DateTime.Now);
+            var passedTasks = tasks.Where(e => e.StartDateTime <= DateTime.Now);
+            return View(new UpcomingPassedTasksViewModel()
+            {
+                UpcomingTasks=upcomingTasks,
+                PassedTasks=passedTasks
+            });
+        }   
+    }
+}
---
+++ Tasks.Web/Models/TaskViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tasks.Web.Models
+{
+    public class TaskViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public TimeSpan? Duration { get; set; }
+        public string Author { get; set; }
+        public string Location { get; set; }
+    }
+}
